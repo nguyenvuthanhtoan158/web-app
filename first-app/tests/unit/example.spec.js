f6e349bb61473a4b889a7603d73b28afcb96feb5
Toan Nguyen <nguyenvuthanhtoan158@gmail.com>
2021-03-07 17:38:08 +0700

    Update configuration for test unit

--- first-app/tests/unit/example.spec.js
+++ first-app/tests/unit/example.spec.js
@@ -1,12 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
 import HelloWorld from '@/components/HelloWorld.vue'
 
+Vue.use(Vuetify);
+
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+  it('renders successfully', () => {
+    const localVue = createLocalVue()
+    shallowMount(HelloWorld, {
+      localVue
     })
-    expect(wrapper.text()).toMatch(msg)
   })
 })
